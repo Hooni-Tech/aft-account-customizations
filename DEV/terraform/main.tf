a3334678981cd4cf1e3decaf111e982096582dea
mhoonkim <mhoonkim@smilegate.com>
2025-11-25 20:11:15 +0900

    정책수정

--- DEV/terraform/main.tf
+++ DEV/terraform/main.tf
@@ -1,10 +1,9 @@
-#Create a budget for S3 with a limit of 50 GB of storage.
-resource "aws_budgets_budget" "s3" {
-  name         = "budget-s3-usage-gb"
-  budget_type  = "USAGE"
-  limit_amount = "50"
-  limit_unit   = "GB"
-  time_period_end   = "2030-12-31_00:00"
-  time_period_start = "2025-01-01_00:00"
-  time_unit    = "MONTHLY"
+resource "aws_budgets_budget" "total_cost" {
+  name              = "budget-total-monthly"
+  budget_type       = "COST"
+  limit_amount      = "100"
+  limit_unit        = "USD"
+  time_period_end   = "2087-06-15_00:00"
+  time_period_start = "2022-02-01_00:00"
+  time_unit         = "MONTHLY"
 }
--- PROD/terraform/main.tf
+++ PROD/terraform/main.tf
@@ -1,36 +1,7 @@
-#Create a budget for ec2 with a limit of certain monthly usage amount.
-resource "aws_budgets_budget" "ec2" {
-  name              = "budget-ec2-monthly"
-  budget_type       = "COST"
-  limit_amount      = "500"
-  limit_unit        = "USD"
-  time_period_end   = "2030-12-31_00:00"
-  time_period_start = "2025-01-01_00:00"
-  time_unit         = "MONTHLY"
-
-  cost_filter {
-    name = "Service"
-    values = [
-      "Amazon Elastic Compute Cloud - Compute",
-    ]
+resource "aws_config_config_rule" "r" {
+  name = "vpc_flow_log_guardrail"
+  source {
+    owner             = "AWS"
+    source_identifier = "VPC_FLOW_LOGS_ENABLED"
   }
-
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = ["dark9121@gmail.com"]
-  }
-}
-
-#Create a budget for s3 with a limit of 3 GB of storage.
-resource "aws_budgets_budget" "s3" {
-  name         = "budget-s3-usage-gb"
-  budget_type  = "USAGE"
-  limit_amount = "10"
-  limit_unit   = "GB"
-  time_period_end   = "2030-12-31_00:00"
-  time_period_start = "2025-01-01_00:00"
-  time_unit    = "MONTHLY"
 }
